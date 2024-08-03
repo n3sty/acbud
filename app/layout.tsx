d9692e8a77f2533044a4f57dc463fee92c482f08
Job Siemerink <siemerinkjob@gmail.com>
2024-08-03 12:52:47 +0200

    vercel analytics integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "./components/auth/SessionWrapper";
 import RecoilContextProvider from "./components/db/RecoilContextProvider";
-import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +21,14 @@ export default function RootLayout({
     <SessionWrapper>
       <RecoilContextProvider>
         <html className="bg-base-200" data-theme="light" lang="en">
-          <meta name="viewport" content="initial-scale=1 user-scalable=no viewport-fit=cover height=device-height width=device-width"></meta>
-          <body className={`${inter.className}`}>{children}</body>
+          <meta
+            name="viewport"
+            content="initial-scale=1 user-scalable=no viewport-fit=cover height=device-height width=device-width"
+          ></meta>
+          <body className={`${inter.className}`}>
+            {children} 
+            <Analytics />
+          </body>
         </html>
       </RecoilContextProvider>
     </SessionWrapper>
