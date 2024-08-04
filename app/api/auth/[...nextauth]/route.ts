425c26280a7789e32867fb828be71905ccddfa7b
Job Siemerink <siemerinkjob@gmail.com>
2024-08-04 02:28:41 +0200

    fix: dev server auth by re-installing packages

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import { db } from "@/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import NextAuth from "next-auth";
 
 import GithubProvider from "next-auth/providers/github";
@@ -14,46 +16,68 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+
   pages: {
     signIn: "/auth/signin",
   },
-  callbacks: {
-    async session({ session, token, user }) {
-      if (session.user && session.user.name) {
-        session.user.username = session.user.name
-          .split(" ")
-          .join("")
-          .toLocaleLowerCase();
 
-        session.user.id = token.sub as string;
+  callbacks: {
+    async signIn({ user, account }) {
+      const userRef = doc(db, "users", user.id as string);
+      const docSnap = await getDoc(userRef);
+
+      console.log("user", user);
+
+      if (!docSnap.exists()) {
+        // Create user in database
+        await setDoc(userRef, {
+          name: user.name,
+          id: user.id,
+          email: user.email,
+          image: user.image,
+          provider: account?.provider,
+          createdAt: new Date(),
+        });
+
+        return true;
+      } else {
+        return true;
+      }
+    },
+
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+
+    async session({ session, token }) {
+      const userRef = doc(db, "users", token.id as string);
+      const docSnap = await getDoc(userRef);
+
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        session.user.email = userData.email;
+        session.user.userProvider = userData.provider;
+        session.user.id = userData.id;
+        session.user.name = userData.name;
+        session.user.image = userData.image;
+      } else {
+        throw new Error("User not found in database");
       }
       return session;
     },
 
-    async redirect({ url, baseUrl }) {
-      const userExists = await checkIfUserExists();
-
-      console.log("Base URL: ", baseUrl)
-      console.log("url: ", url)
-
-      if (userExists) {
-        // Redirect to existing user page
-        if (url.startsWith("/")) return `${baseUrl}${url}`
-        else if (new URL(url).origin === baseUrl) return url
-        return baseUrl
-      } else {
-      // Redirect to new user page
-      return `${baseUrl}/new-user`;
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.image = user.image;
       }
+      return token;
     },
   },
 });
 
 export { handler as GET, handler as POST };
-
-
-function checkIfUserExists() {
-  // throw new Error("Function not implemented.");
-  return true
-}
-
